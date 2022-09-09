e3ceb5f0e7c60f2aaa33c68a7b243a7fbc785812
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-09-09 19:51:43 +0100

    Use authoritative IAM for service accounts

--- terraform/source-repositories.tf
+++ terraform/source-repositories.tf
@@ -15,3 +15,17 @@ resource "google_sourcerepo_repository_iam_member" "writer" {
   role       = "roles/writer"
   member     = "serviceAccount:${google_service_account.source_repositories_github.email}"
 }
+
+data "google_iam_policy" "service_account_source_repositories_github" {
+  binding {
+    role = "roles/iam.workloadIdentityUser"
+    members = [
+      "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/github-pool/attribute.repository/alphagov/govuk-s3-mirror"
+    ]
+  }
+}
+
+resource "google_service_account_iam_policy" "source_repositories_github" {
+  service_account_id = google_service_account.source_repositories_github.name
+  policy_data        = data.google_iam_policy.service_account_source_repositories_github.policy_data
+}
