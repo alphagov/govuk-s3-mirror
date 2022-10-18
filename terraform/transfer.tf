4ee11ac3f639f69bdd1ecf7139ca30d56bbd8861
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-10-18 13:54:06 +0100

    Allow knowledge graph postgres instance access
    The postgres instance of the Knowledge Graph needs access to the bucket
    to download the Publishing API database.

--- terraform/transfer.tf
+++ terraform/transfer.tf
@@ -30,6 +30,7 @@ data "google_iam_policy" "bucket_govuk-integration-database-backups" {
       "group:data-products@digital.cabinet-office.gov.uk",
       "group:data-insights@digital.cabinet-office.gov.uk",
       "serviceAccount:gce-mongodb@govuk-knowledge-graph.iam.gserviceaccount.com",
+      "serviceAccount:gce-postgres@govuk-knowledge-graph.iam.gserviceaccount.com",
       "user:matthew.gregory@digital.cabinet-office.gov.uk",
     ]
   }
